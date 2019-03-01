3cdd51830d5c3d751791253da31890b840b46aa0
Emeka Ananti <emekaananti@gmail.com>
2019-03-01 14:07:31 +0100

    Clear form field after submitting form

--- js/script.js
+++ js/script.js
@@ -4,3 +4,10 @@
 $(document).scroll(function(){
     $("#textBlink").fadeOut().fadeIn("1000");
 });
+
+// Reset form field after clicking Submit Button
+function myFunction() {
+    setTimeout(function(){
+        document.getElementById("resetFormField").reset();
+    }, 5000);
+}
